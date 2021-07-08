7a674ba49fa20b293db01775e78638006262319b
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2021-07-08 21:43:20 +0200

    Added modal example

---
+++ modal-example/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <header class="container container--fluid m-0">
+    <navbar></navbar>
+  </header>
+  <div role="main" class="container container--medium">
+    <button
+      type="button"
+      role="button"
+      class="btn bg--success animate__animated animate__backInDown"
+      v-on:click="showModal()"
+    >
+      Press me to show modal
+    </button>
+  </div>
+  <footer class="container container--fluid m-0 footer bg--dark">
+    <p class="footer__text text--center">Design by Carlos Fernandez Cabrero</p>
+  </footer>
+
+  <div id="modal" class="modal">
+    <div class="modal__content animate__animated animate__backInDown">
+      <span class="modal__close" v-on:click="hideModal()">&times;</span>
+      <h2 class="modal__tittle text--center">Users logged now</h2>
+      <div class="scroll-content">
+        <table class="table">
+          <thead class="table__header">
+            <tr class="table__row">
+              <th class="table-header__item text--left">ID</th>
+              <th class="table-header__item text--left">Name</th>
+              <th class="table-header__item text--left">Last Names</th>
+              <th class="table-header__item text--left">Email</th>
+            </tr>
+          </thead>
+          <tbody class="table__body">
+            <tr class="table__row">
+              <td class="table-body__item">20</td>
+              <td class="table-body__item">Carlos</td>
+              <td class="table-body__item">Jimenez Gonzalez</td>
+              <td class="table-body__item">carlos@gmail.com</td>
+            </tr>
+            <tr class="table__row">
+              <td class="table-body__item">21</td>
+              <td class="table-body__item">Arancha</td>
+              <td class="table-body__item">Benitez Laurejo</td>
+              <td class="table-body__item">arancha@gmail.com</td>
+            </tr>
+            <tr class="table__row">
+              <td class="table-body__item">22</td>
+              <td class="table-body__item">Eugenio</td>
+              <td class="table-body__item">Sanchez Fernandez</td>
+              <td class="table-body__item">eugenio@gmail.com</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/navbar/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+  methods: {
+    showModal() {
+      document.getElementById("modal").style.display = "block";
+    },
+    hideModal() {
+      document.getElementById("modal").style.display = "none";
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+}
+</style>
---
+++ modal-example/src/components/navbar/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="navbar navbar--center bg--dark">
+    <a href="#" class="navbar__brand">
+      <img :src="logoPath" alt="logo" class="navbar__logo" />
+    </a>
+  </div>
+</template>
+
+<script>
+import image from "../../assets/img/mi-logo.png";
+
+import("../../assets/css/styles.css");
+import("../../assets/css/navbar.css");
+
+export default {
+  name: "Navbar",
+  data: function () {
+    return {
+      logoPath: image,
+    };
+  },
+};
+</script>
